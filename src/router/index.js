7c1c290a11847d858f6e2ec8cf41eb8df9ba69b6
Adam Gage Banks-Beard <adam.beard96@icloud.com>
2020-08-01 19:11:15 -0500

    Create UI for looking up station observations. (#1)

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import Index from '../components/Index'
+
+export default {
+    mode: 'history',
+    routes: [
+        {
+            name: 'Index',
+            path: '/',
+            component: Index
+        },
+    ]
+}
